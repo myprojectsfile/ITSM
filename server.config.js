273e8f78afb5ca35456a9091d8d5336c3eac88c0
ahmadi <ahmadi80@gmail.com>
2019-02-16 23:11:41 +0330

    change config file

--- server.config.js
+++ server.config.js
@@ -2,7 +2,8 @@ const ServerConfig = {
   USE_NTLM: false,
   USE_SSPI: true,
   DOMAIN_CONTROLLER: 'ldap://10.1.1.110:389',
-  DOMAIN: 'BPMO.LOCAL'
+  DOMAIN: 'BPMO.LOCAL',
+  DOMAIN_USER_PREFIX: 'BPMO'
 }
 
 module.exports = ServerConfig
